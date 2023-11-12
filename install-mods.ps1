aa3b04d19812ec49d95e8749883ae46d0403b9f7
Rize <georgklose9@gmail.com>
2023-11-12 20:29:20 +0100

    installer now uses sys default java when instance.json doesn't contain one

--- install-mods.ps1
+++ install-mods.ps1
@@ -1,6 +1,12 @@
 $jsonFilePath = "instance.json"
 $jsonContent = Get-Content -Raw -Path $jsonFilePath | ConvertFrom-Json
 
-$java = $jsonContent.java
+## check if java string is "" and if so, set it to the default java Path
+if ($jsonContent.java -eq "") {
+    $jsonContent.java = "java"
+}
+else {
+    $jsonContent.java = $jsonContent.java
+}
 
 Invoke-Expression -Command "$java -jar .\packwiz-installer-bootstrap.jar https://cluelessgroup.github.io/BeepBoopPack/pack.toml"
