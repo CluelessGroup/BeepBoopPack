48000d1a8cc9ce56dd02d6730523f50843ae5c63
Rize <georgklose9@gmail.com>
2023-11-12 20:37:08 +0100

    stoopeed

--- install-mods.ps1
+++ install-mods.ps1
@@ -1,12 +1,11 @@
 $jsonFilePath = "instance.json"
 $jsonContent = Get-Content -Raw -Path $jsonFilePath | ConvertFrom-Json
 
-## check if java string is "" and if so, set it to the default java Path
 if ($jsonContent.java -eq "") {
-    $jsonContent.java = "java"
+    $java = "java"
 }
 else {
-    $jsonContent.java = $jsonContent.java
+    $java = $jsonContent.java
 }
 
 Invoke-Expression -Command "$java -jar .\packwiz-installer-bootstrap.jar https://cluelessgroup.github.io/BeepBoopPack/pack.toml"
